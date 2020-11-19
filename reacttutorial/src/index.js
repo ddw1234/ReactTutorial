ae654c2738e356040878c57596cc5cf505017696
Dinesh-PC <dineshwagh777@gmail.com>
2020-11-19 19:10:08 +0530

    Part 10 Use of State

--- reacttutorial/src/index.js
+++ reacttutorial/src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+class Employee extends Component{
+
+state ={
+    name:this.props.name
+}
+
+    render(){
+        return(
+            <div>
+                <h1>Hello {this.state.name}</h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<Employee name="Dinesh Wagh"/>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
