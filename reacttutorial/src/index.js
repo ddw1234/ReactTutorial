ecc44ba2ee8a2cc91c4575833470f9b3dcd495af
Dinesh-PC <dineshwagh777@gmail.com>
2020-11-20 01:56:26 +0530

    Part 14 Update state

--- reacttutorial/src/index.js
+++ reacttutorial/src/index.js
@@ -7,33 +7,46 @@ import * as serviceWorker from './serviceWorker';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 class Employee extends Component{
+   
     constructor(props){
+
         super(props);
-        this.state ={
-            name:this.props.name,
-            count:this.props.count,
+
+        this.state={
+         name:this.props.name,
+         empid:this.props.empid,
+         salary:this.props.salary
         };
-
-        this.Clickme=this.Clickme.bind(this);
     }
 
-    Clickme(){
-        let count=this.state.count;
-        this.setState({count: ++count})
+    Updatestatevalue =()=>{
+        let name=this.state.name;
+        let empid=this.state.empid;
+        let salary=this.state.salary;
+
+        this.setState({
+            name:"Dinesh1",
+         empid:"2",
+         salary:3000
+        });
     }
+
+
 
     render(){
         return(
             <div>
-                <h1>Hello {this.state.name}</h1>
-                <h1>Count= {this.state.count}</h1>
-                <button onClick={this.Clickme}>Click to increment</button>
+                <h1>Hello Employee</h1>
+                <h1>Emplyee name : {this.state.name}</h1>
+                <h1>Emplyee empid : {this.state.empid}</h1>
+                <h1>Emplyee salary : {this.state.salary}</h1>
+                <button onClick={this.Updatestatevalue}>Update info</button>
             </div>
         )
     }
 }
 
-ReactDOM.render(<Employee name="Dinesh Wagh" count="0"/>,document.getElementById('root'));
+ReactDOM.render(<Employee  name="dinesh1" empid="2" salary="2000" />,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
