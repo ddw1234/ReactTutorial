edc4bf53605835246829d7e2df1ab4393faf4e33
Dinesh-PC <dineshwagh777@gmail.com>
2020-11-19 19:55:19 +0530

    Part 10 make changes related to state

--- reacttutorial/src/index.js
+++ reacttutorial/src/index.js
@@ -7,21 +7,33 @@ import * as serviceWorker from './serviceWorker';
 // ReactDOM.render(<App />, document.getElementById('root'));
 
 class Employee extends Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            name:this.props.name,
+            count:this.props.count,
+        };
 
-state ={
-    name:this.props.name
-}
+        this.Clickme=this.Clickme.bind(this);
+    }
+
+    Clickme(){
+        let count=this.state.count;
+        this.setState({count: ++count})
+    }
 
     render(){
         return(
             <div>
                 <h1>Hello {this.state.name}</h1>
+                <h1>Count= {this.state.count}</h1>
+                <button onClick={this.Clickme}>Click to increment</button>
             </div>
         )
     }
 }
 
-ReactDOM.render(<Employee name="Dinesh Wagh"/>,document.getElementById('root'));
+ReactDOM.render(<Employee name="Dinesh Wagh" count="0"/>,document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
