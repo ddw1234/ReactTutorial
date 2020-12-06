9973ebb7f40d09fb9358890a4c1fd869d38ee137
Dinesh-PC <dineshwagh777@gmail.com>
2020-12-06 22:34:19 +0530

    Part 34 use of Ref() frunction

--- reacttutorial/src/index.js
+++ reacttutorial/src/index.js
@@ -1,45 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class NameForm extends React.Component {
-  constructor(props) {
+class NameForm extends React.Component{
+  constructor(props){
     super(props);
-    this.state = {value: ''};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleSubmit=this.handleSubmit.bind(this);
+    this.input=React.createRef();
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+  //To write an uncontrolled component, instead of writing an event handler for every state update, you can use a ref to get form values from the DOM.
+  
+  handleSubmit(event){
+    alert('Current value is ' + this.input.current.value);
     event.preventDefault();
-/*
-The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
-For example, this can be useful when:
- Clicking on a "Submit" button, prevent it from submitting a form
- Clicking on a link, prevent the link from following the URL
-*/
   }
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+  render(){
+    return(
+     <form onSubmit={this.handleSubmit}>
+       <label>
+         Name : <input type="text" ref={this.input}></input>
+       </label>
+       <input type="submit" value="Submit"></input> 
+     </form>
     );
   }
+
 }
 
+
 ReactDOM.render(<NameForm/>,document.getElementById("root"));
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
