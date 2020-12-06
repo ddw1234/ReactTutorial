2eb4afa24a9ab54ba9f4d2b2aba6c559b5b8f206
Dinesh-PC <dineshwagh777@gmail.com>
2020-12-06 21:49:49 +0530

    Part 33 Form Handling

--- reacttutorial/src/index.js
+++ reacttutorial/src/index.js
@@ -1,26 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Reactact(){
+class NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
 
-  const arr=[1,2,3,4,5];
-  const newarr=arr.map((val,index)=>{
-    // return val;
-    // map() function is used to iterate through array and list 
-    return <li key={index}>{val+1}</li>;
-  });
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-  console.log(arr);
- return(
-   <>
-   <h1>Hello</h1>
-   <ul>{newarr}</ul>
-   </>
- )
-  
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+/*
+The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
+For example, this can be useful when:
+ Clicking on a "Submit" button, prevent it from submitting a form
+ Clicking on a link, prevent the link from following the URL
+*/
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
 
-ReactDOM.render(<Reactact/>,document.getElementById("root"));
+ReactDOM.render(<NameForm/>,document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
