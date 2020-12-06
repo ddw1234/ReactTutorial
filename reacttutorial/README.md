f3c3d4524bf2f695f8b8a22e949934e8c4d34a02
Dinesh-PC <dineshwagh777@gmail.com>
2020-12-06 23:04:14 +0530

    Part 35 Integrate Bootstrap with React.js

--- reacttutorial/README.md
+++ reacttutorial/README.md
@@ -13,4 +13,6 @@ npx create-react-app reacttutorial
 4. run below command to run the project
 npm start
 5. Add the .gitignore file into reacttutorial project so you can commit only required files.
+6. To import React-bootstrap use below command
+npm install react-bootstrap bootstrap
 
--- reacttutorial/src/index.js
+++ reacttutorial/src/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import BootForm from './form';
 
 class NameForm extends React.Component{
-  constructor(props){
-    super(props);
-    this.handleSubmit=this.handleSubmit.bind(this);
-    this.input=React.createRef();
-  }
-
-  //To write an uncontrolled component, instead of writing an event handler for every state update, you can use a ref to get form values from the DOM.
-  
-  handleSubmit(event){
-    alert('Current value is ' + this.input.current.value);
-    event.preventDefault();
-  }
-
+ 
   render(){
     return(
-     <form onSubmit={this.handleSubmit}>
-       <label>
-         Name : <input type="text" ref={this.input}></input>
-       </label>
-       <input type="submit" value="Submit"></input> 
-     </form>
+     <>
+     <div className="container">
+     <h1>Welcome to the website</h1>
+     <BootForm/>
+     </div>
+     </>
     );
   }
 
