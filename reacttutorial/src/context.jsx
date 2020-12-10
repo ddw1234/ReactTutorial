96036ea3d43e574d7f8a63150cfe851b8c328e21
Dinesh-PC <dineshwagh777@gmail.com>
2020-12-11 01:02:49 +0530

    Part 39 Context Type in React JS

--- reacttutorial/src/context.jsx
+++ reacttutorial/src/context.jsx
@@ -1,4 +1,3 @@
 import React from 'react';
 export const MyContext=React.createContext();
 export const Provider=MyContext.Provider;
-export const Consumer=MyContext.Consumer; 
--- reacttutorial/src/liftingstate/salary.jsx
+++ reacttutorial/src/liftingstate/salary.jsx
@@ -1,19 +1,20 @@
 import React,{Component} from 'react';
-import {Consumer} from '../context';
+import {MyContext} from '../context';
 
 class Salary extends Component{
 
+    static contextType=MyContext;
+
     render(){
+     
+        const {salary,clickChange}=this.context;
+
         return(
             <>
-            <h1>Salary Details</h1>
-            <Consumer>
-        {({salary,clickChange})=>
-        <>
+            <h1>Salary Details</h1>          
         <h1>Salary : {salary}</h1>
         <button onClick={clickChange}>Change Salary</button>
-        </>}
-      </Consumer>
+        
             </>
         )
     }
